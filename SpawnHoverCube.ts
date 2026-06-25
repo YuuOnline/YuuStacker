60407687c1fd42d0700f49a778a95a24503430e1
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-24 17:19:28 -0700

    Spawning 32 hover cubes

---
+++ SpawnHoverCube.ts
@@ -0,0 +1,19 @@
+import { Color } from "./Yuu API/Basic Types/Color";
+import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
+import { Vector3 } from "./Yuu API/Basic Types/Vector3"
+import { spawnPrimitive } from "./Yuu API/SpawnPrimitive"
+
+
+export const spawnHoverCube = {
+  spawn,
+}
+
+
+// Know where other blocks are so they don't overlap
+
+const diameter = 8;
+const scale = 0.35;
+
+function spawn() {
+  spawnPrimitive.cube(new Vector3(Math.random() * diameter, 1, Math.random() * diameter), new Vector3(scale, scale, scale), Quaternion.fromEuler(new Vector3(0, Math.random(), 0)), Color.randomHue(), 0.75, false, 'Animated', undefined);
+}
--- StartGame.ts
+++ StartGame.ts
@@ -1,3 +1,4 @@
+import { spawnHoverCube } from "./SpawnHoverCube";
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
 import { registerStart } from "./Yuu API/RegisterStart";
@@ -7,5 +8,15 @@ registerStart(start);
 function start() {
   inWorldConsole.visible(true, new Vector3(0, 1.5, -1.5));
 
-  console.log('Hello Video!!!');
+  console.log('Calling startNewGame!');
+
+  startNewGame();
+}
+
+const count = 32;
+
+function startNewGame() {
+  for (let i = 0; i < count; i++) {
+    spawnHoverCube.spawn();
+  }
 }
