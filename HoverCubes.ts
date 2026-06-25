a01fa9c47ff9bea652dc05329ad96463038b8eb7
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-24 17:44:41 -0700

    Added more spaces to grid

--- HoverCubes.ts
+++ HoverCubes.ts
@@ -24,8 +24,8 @@ function destroyPreviousCubes() {
   gridPositionsOccupied.length = 0;
 }
 
-const gridSpaceDiameter = 0.35;
-const numberOfTilesOutwards = 8;
+const gridSpaceDiameter = 0.45;
+const numberOfTilesOutwards = 12;
 const scale = 0.25;
 
 function spawn() {
@@ -38,7 +38,7 @@ function spawn() {
 
   let i = 0;
 
-  while (gridPositionsOccupied.includes(key) && i < 64) {
+  while (gridPositionsOccupied.includes(key) && i < 256) {
     xCoord = Math.floor(Math.random() * ((numberOfTilesOutwards * 2) + 1)) - numberOfTilesOutwards;
     zCoord = Math.floor(Math.random() * (numberOfTilesOutwards + 1));
 
@@ -49,7 +49,7 @@ function spawn() {
   
   gridPositionsOccupied.push(key);
   
-  const pos = new Vector3(xCoord * gridSpaceDiameter, yCoord, zCoord * gridSpaceDiameter);
+  const pos = new Vector3(xCoord * gridSpaceDiameter, yCoord, -zCoord * gridSpaceDiameter);
 
   const cube = spawnPrimitive.cube(pos, new Vector3(scale, scale, scale), Quaternion.fromEuler(new Vector3(0, Math.random(), 0)), Color.randomHue(), 0.75, false, 'Animated', undefined);
 
